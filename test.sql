9e5e780af8ff08cc4da533a269398cb04211c60a
Arian Bessonart <arianbessonart@gmail.com>
2016-06-12 19:55:13 -0300

    Move config settings to a new file. Change goToProvider to export functions

--- test.sql
+++ test.sql
@@ -13,6 +13,7 @@ DECLARE
 
 BEGIN
 	c_fact_params_data = '';
+  
 	SELECT m_oo_fact_id, m_notification_type_id, m_phone, m_event_type_service_id, m_user_id, m_tracking_number, m_ts_incoming_ofct, m_origin_id, m_incoming_fact_data, m_specific_fact_data, m_incoming_fact_routine_data FROM md_get_oo_fact_fact_routine_data_by_fact_routine_id(p_operator_id, p_date_partition, p_fact_routine_id) INTO v_oo_fact_id, v_notication_type_id, v_phone, v_event_type_service_id, v_user_id, v_tracking_number, v_ts_incoming, v_origin_id, c_fact_incoming_data, c_fact_specific_data, c_fact_rtn_incoming_data;
 
 		c_fact_params_data = c_fact_params_data || hstore(ARRAY['notificationTypeId', v_notication_type_id::text, 'operatorId', p_operator_id::text, 'eventTypeServiceId', v_event_type_service_id::text, 'ooFactId', v_oo_fact_id::text, 'datePartition', p_date_partition::text, 'tsNotificationFact'::text, v_ts_incoming::text, 'phone', v_phone, 'userId', v_user_id::text, 'originId', v_origin_id::text, 'trackingNumber', v_tracking_number, 'routineOoFactId', p_fact_routine_id::text]);
